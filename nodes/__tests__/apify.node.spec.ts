8b47d5dc2e704440a1256b99521d08b45372addc
Marina Jakčin <marina.jakcin@applifting.cz>
2025-05-13 09:26:37 +0200

    fix: formatting

--- nodes/__tests__/apify.node.spec.ts
+++ nodes/__tests__/apify.node.spec.ts
@@ -1,24 +1,24 @@
 import { Apify } from '../Apify/Apify.node';
 
 describe('Apify Node', () => {
-  let apifyNode: Apify;
+	let apifyNode: Apify;
 
-  beforeEach(() => {
-    apifyNode = new Apify();
-  });
+	beforeEach(() => {
+		apifyNode = new Apify();
+	});
 
-  describe('description', () => {
-    it('should have a name property', () => {
-      expect(apifyNode.description.name).toBeDefined();
-      expect(apifyNode.description.name).toEqual('apify');
-    });
+	describe('description', () => {
+		it('should have a name property', () => {
+			expect(apifyNode.description.name).toBeDefined();
+			expect(apifyNode.description.name).toEqual('apify');
+		});
 
-    it('should have properties defined', () => {
-      expect(apifyNode.description.properties).toBeDefined();
-    });
+		it('should have properties defined', () => {
+			expect(apifyNode.description.properties).toBeDefined();
+		});
 
-    it('should have credential properties defined', () => {
-      expect(apifyNode.description.credentials).toBeDefined();
-    });
-  });
-}); 
+		it('should have credential properties defined', () => {
+			expect(apifyNode.description.credentials).toBeDefined();
+		});
+	});
+});
