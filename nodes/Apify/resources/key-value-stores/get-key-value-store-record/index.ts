99501ef37608ac837e2254e9388817c57bc668d3
Denny Nguyen <55693964+ngut37@users.noreply.github.com>
2025-05-22 17:22:12 +0200

    fix: overhaul get key-value to programatic style

--- nodes/Apify/resources/key-value-stores/get-key-value-store-record/index.ts
+++ nodes/Apify/resources/key-value-stores/get-key-value-store-record/index.ts
@@ -6,19 +6,13 @@ import * as helpers from '../../../helpers';
 import { properties as rawProperties } from './properties';
 import { runHooks } from './hooks';
 
-export const name = 'Get Key-value Store Record';
+export const name = 'Get Key-Value Store Record';
 
 const rawOption: INodePropertyOptions = {
-	name: 'Get Key-value Store Record',
-	value: 'Get Key-value Store Record',
-	action: 'Get Key-value Store Record',
-	description: 'Gets a value stored in the key-value store under a specific key.',
-	routing: {
-		request: {
-			method: 'GET',
-			url: '=/v2/key-value-stores/{{$parameter["storeId"]}}/records/{{$parameter["recordKey"]}}',
-		},
-	},
+	name: name,
+	value: name,
+	action: name,
+	description: 'Gets a value stored in the key-value store under a specific key',
 };
 
 const { properties, option } = runHooks(rawOption, rawProperties);
