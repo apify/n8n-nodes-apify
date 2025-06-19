e7699da7a59236e8faae218f88df5ded1c9f5d41
Denny Nguyen <55693964+ngut37@users.noreply.github.com>
2025-06-19 04:21:39 +0200

    test: create trigger-node tests

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/nodes', '<rootDir>/credentials'],
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'js', 'json'],
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	roots: ['<rootDir>/nodes', '<rootDir>/credentials'],
+	// testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+	testMatch: ['**/ApifyTrigger.node.spec.ts'],
+	transform: {
+		'^.+\\.ts$': 'ts-jest',
+	},
+	moduleFileExtensions: ['ts', 'js', 'json'],
 };
--- nodes/Apify/__tests__/utils/runWebhookMethod.ts
+++ nodes/Apify/__tests__/utils/runWebhookMethod.ts
@@ -4,11 +4,12 @@ import {
 	WebhookSetupMethodNames,
 	Workflow,
 	IWebhookData,
-	IHttpRequestMethods,
 	IWebhookDescription,
-	INode,
 } from 'n8n-workflow';
 import { NodeExecuteFunctions } from 'n8n-core';
+import { INodeExecuteFunctions } from 'n8n-workflow';
+import { WorkflowExecuteMode } from 'n8n-workflow';
+import { WorkflowActivateMode } from 'n8n-workflow';
 
 export const runWebhookMethod = async (
 	workflow: Workflow,
@@ -21,28 +22,65 @@ export const runWebhookMethod = async (
 		additionalData,
 	);
 
+	// const nodesKeys = Object.keys(workflow.nodes);
+
+	// const node = workflow.nodes[nodesKeys[0]];
+
 	const webhookDescription: IWebhookDescription = {
 		name: 'default',
-		httpMethod: method as IHttpRequestMethods,
-		path: '/webhook',
+		httpMethod: 'POST',
+		responseMode: 'onReceived',
+		path: 'webhook',
 	};
 
 	const webhookData: IWebhookData = {
-		httpMethod: method as IHttpRequestMethods,
-		node: webhookNode.webhookId || 'test',
-		path: '/webhook',
+		httpMethod: 'POST',
+		node: webhookNode.node,
+		path: 'webhook',
 		webhookDescription,
 		workflowId: workflow.id,
 		workflowExecuteAdditionalData: additionalData,
 	};
 
-	const result = await workflow.runWebhook(
-		webhookData,
-		webhookNode as unknown as INode,
-		additionalData,
-		NodeExecuteFunctions,
-		'manual',
-	);
+	// const result = await workflow.runWebhookMethod(
+	//   method,
+	//   webhookNode,
+	//   NodeExecuteFunctions,
+	//   'manual',
+	//   'manual',
+	//   true,
+	// );
 
-	return result;
+	try {
+		// const result = await (
+		// 	workflow as unknown as Record<string, any> & {
+		// 		runWebhookMethod: (
+		// 			method: WebhookSetupMethodNames,
+		// 			webhookData: IWebhookData,
+		// 			nodeExecuteFunctions: INodeExecuteFunctions,
+		// 			mode: WorkflowExecuteMode,
+		// 			activation: WorkflowActivateMode,
+		// 			isTest?: boolean,
+		// 		) => Promise<boolean | undefined>;
+		// 	}
+		// ).runWebhookMethod(webhookData, node, additionalData, NodeExecuteFunctions, 'manual');
+
+		const result = await (
+			workflow as unknown as Record<string, any> & {
+				runWebhookMethod: (
+					method: WebhookSetupMethodNames,
+					webhookData: IWebhookData,
+					nodeExecuteFunctions: INodeExecuteFunctions,
+					mode: WorkflowExecuteMode,
+					activation: WorkflowActivateMode,
+					isTest?: boolean,
+				) => Promise<boolean | undefined>;
+			}
+		).runWebhookMethod(method, webhookData, NodeExecuteFunctions, 'manual', 'manual', true);
+
+		return result;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
 };
