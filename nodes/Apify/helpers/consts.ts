c947edcfd4e969b064a45ca95fb72a0f94998673
Denny Nguyen <55693964+ngut37@users.noreply.github.com>
2025-06-30 14:13:35 +0200

    feat: poll run status in run-actor

--- nodes/Apify/helpers/consts.ts
+++ nodes/Apify/helpers/consts.ts
@@ -1,2 +1,8 @@
 export const WEB_CONTENT_SCRAPER_ACTOR_ID = 'aYG0l9s7dbB7j3gbS';
 export const APIFY_API_URL = 'https://api.apify.com';
+// https://github.com/apify/apify-shared-js/blob/5fbf2927e083c2ddcaece5e87eb07492ff89fc50/packages/consts/src/consts.ts#L53
+export const TERMINAL_RUN_STATUSES = ['SUCCEEDED', 'FAILED', 'TIMED-OUT', 'ABORTED'];
+// 1 second
+export const WAIT_FOR_FINISH_POLL_INTERVAL = 1000;
+// 300 * poll-interval = 5 minutes
+export const WAIT_FOR_FINISH_POLL_ATTEMPTS = 300;
