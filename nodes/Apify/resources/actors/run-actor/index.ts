3040717f173c02f5d5f3f112c2662ce2b8ed73c1
Denny Nguyen <55693964+ngut37@users.noreply.github.com>
2025-08-15 11:29:23 +0200

    fix: rewrite false descriptions

--- nodes/Apify/resources/actors/run-actor/index.ts
+++ nodes/Apify/resources/actors/run-actor/index.ts
@@ -9,7 +9,8 @@ const rawOption: INodePropertyOptions = {
 	name: 'Run an Actor',
 	value: 'Run actor',
 	action: 'Run an Actor',
-	description: 'Runs an Actor and immediately returns without waiting for the run to finish',
+	description:
+		'Runs an Actor. You can optionally override the Actor’s input configuration by providing a custom body, this body will override the prefilled input values.',
 };
 
 const { properties, option } = runHooks(rawOption, rawProperties);
