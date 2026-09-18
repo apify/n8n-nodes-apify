45add67d98b79d3c0cfe8ad55ecf9f2c4d7d49bb
Gokdeniz Kaymak <gokdeniz.kaymak@apify.com>
2026-09-18 15:00:25 +0200

    fix: dark logo fix

--- nodes/Apify/Apify.node.ts
+++ nodes/Apify/Apify.node.ts
@@ -16,7 +16,7 @@ export class Apify implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'Apify',
 		name: 'apify',
-		icon: { light: 'file:apify-light.svg', dark: 'file:apify-dark.svg' },
+		icon: 'file:apify-light.svg',
 		group: ['transform'],
 		version: [1, 2],
 		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
